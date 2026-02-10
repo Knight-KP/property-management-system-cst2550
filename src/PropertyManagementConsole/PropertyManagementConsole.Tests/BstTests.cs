7e0a8b49f9a0212f8151957ea72d16a6ed3e0fac
Knight-KP <kp972@live.mdx.ac.uk>
2026-02-10 11:39:42 +0000

    chore: import baseline project into src

---
+++ src/PropertyManagementConsole/PropertyManagementConsole.Tests/BstTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PropertyManagementConsole.DataStructures;
+
+namespace PropertyManagementConsole.Tests;
+
+[TestClass]
+public class BstTests
+{
+    [TestMethod]
+    public void InsertAndSearch_FindsInsertedValues()
+    {
+        var bst = new BinarySearchTree<string>();
+        bst.Insert(2, "B");
+        bst.Insert(1, "A");
+        bst.Insert(3, "C");
+
+        Assert.AreEqual("A", bst.Search(1));
+        Assert.AreEqual("B", bst.Search(2));
+        Assert.AreEqual("C", bst.Search(3));
+    }
+
+    [TestMethod]
+    public void Search_MissingKey_ReturnsNull()
+    {
+        var bst = new BinarySearchTree<string>();
+        bst.Insert(10, "X");
+
+        var result = bst.Search(99);
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public void Insert_SameKey_OverridesValue()
+    {
+        var bst = new BinarySearchTree<string>();
+        bst.Insert(5, "Old");
+        bst.Insert(5, "New");
+
+        Assert.AreEqual("New", bst.Search(5));
+    }
+}
---
+++ src/PropertyManagementConsole/PropertyManagementConsole.Tests/InvoiceCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PropertyManagementConsole.Models;
+using PropertyManagementConsole.Utils;
+
+namespace PropertyManagementConsole.Tests;
+
+[TestClass]
+public class InvoiceCalculatorTests
+{
+    [TestMethod]
+    public void CalculateExtrasTotal_NoJobs_ReturnsZero()
+    {
+        var jobs = new List<MaintenanceJob>();
+
+        var extras = InvoiceCalculator.CalculateExtrasTotal(jobs);
+
+        Assert.AreEqual(0m, extras);
+    }
+
+    [TestMethod]
+    public void CalculateExtrasTotal_MultipleJobs_SumsCorrectly()
+    {
+        var jobs = new List<MaintenanceJob>
+        {
+            new MaintenanceJob { Cost = 80m },
+            new MaintenanceJob { Cost = 120m },
+            new MaintenanceJob { Cost = 45.50m }
+        };
+
+        var extras = InvoiceCalculator.CalculateExtrasTotal(jobs);
+
+        Assert.AreEqual(245.50m, extras);
+    }
+
+    [TestMethod]
+    public void CalculateGrandTotal_AddsBaseAndExtras()
+    {
+        decimal baseRent = 950m;
+        decimal extras = 120m;
+
+        decimal total = InvoiceCalculator.CalculateGrandTotal(baseRent, extras);
+
+        Assert.AreEqual(1070m, total);
+    }
+}
